43e1c8ca55db5fcec244585bb0daceef55c1fc00
Rob Jewell <jewell.robertp@gmail.com>
2016-09-10 16:36:44 -0400

    draw route to create a selector and corresponding endpoint. also create endpoint to get a selector by hostname

--- routes/api/selectors.js
+++ routes/api/selectors.js
@@ -1,5 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var gcloud = require('gcloud');
+
+var datastore = gcloud.datastore({
+  projectId: 'prototype-content-consumer',
+  keyFilename: './credentials/prototype-content-consumer-fb8666071546.json'
+});
+
+/* POST /api/v1/selectors */
+router.post('/', function(req, res, next) {
+  let selectorKey = datastore.key('Selector');
+  datastore.save({
+    key: selectorKey,
+    data: req.body
+  }, function (err) {
+    if (err) {
+      return res.json({errors: req});
+    }
+  });
+
+
+  res.json({
+    "data": req.body,
+    "jsonapi": {
+      "version": "1.0.0"
+    }
+  });
+})
+
+/* GET /api/v1/selectors/:hostname */
+router.get('/:hostname', function(req, res, next) {
+  let data;
+  let query = datastore.createQuery('Selector').filter('hostname', req.params.hostname);;
+  datastore.runQuery(query, function (err, selector) {
+    if (!err) {
+      res.json({
+        "data": selector[0].data,
+        "jsonapi": {
+          "version": "1.0.0"
+        }
+      });
+    }
+  });
+})
+
 
 /* GET /api/v1/selectors */
 router.get('/', function(req, res, next) {
